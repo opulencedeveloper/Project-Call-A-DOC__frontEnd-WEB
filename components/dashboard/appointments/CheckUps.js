4ba83d544e5413a5d18efcebb35fd25efd7eb471
opulencedeveloper <victorkudos@gmail.com>
2023-05-23 19:06:22 +0100

    update-30

---
+++ components/dashboard/appointments/CheckUps.js
@@ -0,0 +1,108 @@
+import Image from "next/image";
+
+import styles from "./CheckUps.module.css";
+
+const { table_spacing } = styles;
+
+const checkUps = [
+  {
+    name: "Jenny Wilson",
+    date: "15/08/2017",
+    time: "7:30 am",
+    status: "Ongoing",
+    nextCheckup: "15/08/2017",
+  },
+  {
+    name: "Jenny Wilson",
+    date: "15/08/2017",
+    time: "7:30 am",
+    status: "Completed",
+    nextCheckup: "15/08/2017",
+  },
+  {
+    name: "Jenny Wilson",
+    date: "15/08/2017",
+    time: "7:30 am",
+    status: "Ongoing",
+    nextCheckup: "15/08/2017",
+  },
+  {
+    name: "Jenny Wilson",
+    date: "15/08/2017",
+    time: "7:30 am",
+    status: "Ongoing",
+    nextCheckup: "15/08/2017",
+  },
+];
+
+const Checkups = () => {
+  return (
+    <div>
+      <div className="mb-5">Checkups</div>
+      <div className="border border-ash6 flex rounded-lg mr-2 h-12 w-64">
+        <Image
+          src="/images/icon/search-gray-shade.svg"
+          alt="search-icon"
+          className="w-auto h-auto p-3.5"
+          width={16}
+          height={16}
+        />
+        <input
+          type="text"
+          className="py-4 w-full rounded-lg placeholder-ash6 text-base font-light focus:outline-none"
+          placeholder="Search"
+        />{" "}
+      </div>
+
+      <table className={`min-w-full ${table_spacing}`}>
+        <thead>
+          <tr className="text-ash2">
+            <th className="text-start font-normal text-sm text-ash2 pl-5">
+              Doctor
+            </th>
+            <th className="text-start font-normal text-sm">Date</th>
+            <th className="text-start font-normal text-sm">Time</th>
+            <th className="text-start font-normal text-sm">Status</th>
+            <th className="text-start font-normal text-sm">Next checkup</th>
+            <th className="text-start font-normal text-sm">Action</th>
+          </tr>
+        </thead>
+        <tbody className="mt-20 py-10">
+          {checkUps.map((checkUpData, index) => {
+            const oddNumber = index % 2 !== 0;
+            const color = !oddNumber ? "bg-ash3" : ""; //TODO=> refactoring with Calculus
+            return (
+              <tr key={index} className={color}>
+                <td className="flex items-center h-16 py-2 pl-5 space-x-7">
+                  <Image
+                    src="/images/doctor11.svg"
+                    alt="doctor11"
+                    className="w-10 h-10"
+                    width={48}
+                    height={48}
+                  />
+                  <p>{checkUpData.name}</p>
+                </td>
+                <td>{checkUpData.date}</td>
+                <td>{checkUpData.time}</td>
+                <td>{checkUpData.status}</td>
+                <td>{checkUpData.nextCheckup}</td>
+                <td>
+                  <Image
+                    src="/images/icon/three-dot-vert.svg"
+                    alt="three-dot-icon"
+                    className="w-auto h-auto"
+                    width={20}
+                    height={20}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Checkups;
