251938880a942c7910cf9ace2b1c8969e71fbbe9
Amobi Victor Chukwuka <119549641+opulencedeveloper@users.noreply.github.com>
2023-06-18 21:27:50 +0100

    Update index.js

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -2,7 +2,7 @@ import MyLayout from "@/components/layout/MyLayout";
 import SectionOne from "@/components/about-us/section-one";
 import SectionThree from "@/components/about-us/section-three";
 import SectionTwo from "@/components/about-us/section-two";
-
+//update
 const AboutUs = () => {
   return (
     <MyLayout>
