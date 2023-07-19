e59620003e598d322be7c684b2a827da3ac7bce1
opulencedeveloper <victorkudos@gmail.com>
2023-07-19 17:54:26 +0100

    updated video call error

--- pages/room/[roomid].js
+++ pages/room/[roomid].js
@@ -1,91 +1,50 @@
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
-let myMeeting;
+//import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import { useState, useEffect, useCallback } from "react";
 
 const Room = () => {
   const router = useRouter();
   const roomid = router.query.roomid;
   const userId = Date.now().toString();
   const userName = "Victor Opulence";
-
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    myMeeting = async (element) => {
-      const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
+    setMounted(true);
+  }, []);
 
-      const appId = 2145588405;
-      const serverSecret = "bf10471bd0b2a4775ee32559fe80afb4";
-      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-        appId,
-        serverSecret,
-        roomid,
-        userId,
-        userName
-      );
+  if (!mounted || typeof document === "undefined") {
+    return null;
+  }
 
-      const zc = ZegoUIKitPrebuilt.create(kitToken);
-      zc.joinRoom({
-        container: element,
-        sharedLinks: [
-            { name: "Copy Link", url: `http://localhost:4000/room/${roomid}` },
-          ],
-          scenario: {
-            mode: ZegoUIKitPrebuilt.OneONoneCall,
-          },
-          showScreenSharingButton: false,
-      });
-    };
+  const myMeeting = async (element) => {
+    const { ZegoUIKitPrebuilt } = await import(
+      "@zegocloud/zego-uikit-prebuilt"
+    );
+    const appId = 2145588405;
+    const serverSecret = "bf10471bd0b2a4775ee32559fe80afb4";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appId,
+      serverSecret,
+      roomid,
+      userId,
+      userName
+    );
 
-  }, [roomid, userId, userName]);
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    zc.joinRoom({
+      container: element,
+      sharedLinks: [
+        { name: "Copy Link", url: `http://localhost:4000/room/${roomid}` },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: false,
+    });
+  };
 
-  return (
-    <div>
-      <div ref={myMeeting} />
-    </div>
-  );
+  return <div>{mounted ? <div ref={myMeeting} /> : <p>Please wait</p>}</div>;
 };
 
 export default Room;
-
-
-
-
-// import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-// import { useEffect } from "react";
-
-// const Room = () => {
-//   const router = useRouter();
-//   const roomid = router.query.roomid;
-//   const userId = Date.now().toString();
-//   const userName = "Victor Opulence";
-
-//   const myMeeting = async (element) => {
-//     const appId = 2145588405;
-//     const serverSecret = "bf10471bd0b2a4775ee32559fe80afb4";
-//     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-//       appId,
-//       serverSecret,
-//       roomid,
-//       userId,
-//       userName
-//     );
-
-//     const zc = ZegoUIKitPrebuilt.create(kitToken);
-//     zc.joinRoom({
-//       container: element,
-//       sharedLinks: [
-//         { name: "Copy Link", url: `http://localhost:4000/room/${roomid}` },
-//       ],
-//       scenario: {
-//         mode: ZegoUIKitPrebuilt.OneONoneCall,
-//       },
-//       showScreenSharingButton: false,
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <div ref={myMeeting} />
-//     </div>
-//   );
-// };
