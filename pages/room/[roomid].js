aeafa17cd4104ad41be8fa10589a33ae8e050366
opulencedeveloper <victorkudos@gmail.com>
2023-07-19 15:47:08 +0100

    fixed video call bug

--- pages/room/[roomid].js
+++ pages/room/[roomid].js
@@ -1,47 +1,50 @@
 import { useRouter } from "next/router";
-import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const Room = () => {
   const router = useRouter();
   const roomid = router.query.roomid;
   const userId = Date.now().toString();
   const userName = "Victor Opulence";
+  const elementRef = useRef(null);
 
   useEffect(() => {
-    const myMeeting = async (element) => {
-      const appId = 2145588405;
-      const serverSecret = "bf10471bd0b2a4775ee32559fe80afb4";
-      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-        appId,
-        serverSecret,
-        roomid,
-        userId,
-        userName
-      );
+    const loadZegoUIKit = async () => {
+      const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
 
-      const zc = ZegoUIKitPrebuilt.create(kitToken);
-      zc.joinRoom({
-        container: element,
-        sharedLinks: [
-          { name: "Copy Link", url: `https://calladoc247.vercel.app/room/${roomid}` },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.OneONoneCall,
-        },
-        showScreenSharingButton: false,
-      });
+      const myMeeting = async (element) => {
+        const appId = 2145588405;
+        const serverSecret = "bf10471bd0b2a4775ee32559fe80afb4";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+          appId,
+          serverSecret,
+          roomid,
+          userId,
+          userName
+        );
+
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+          container: element,
+          sharedLinks: [
+            { name: "Copy Link", url: `http://localhost:4000/room/${roomid}` },
+          ],
+          scenario: {
+            mode: ZegoUIKitPrebuilt.OneONoneCall,
+          },
+          showScreenSharingButton: false,
+        });
+      };
+
+      myMeeting(elementRef.current);
     };
 
-    if (typeof window !== "undefined") {
-      const element = document.querySelector("#myMeetingContainer");
-      myMeeting(element);
-    }
+    loadZegoUIKit();
   }, [roomid, userId, userName]);
 
   return (
     <div>
-      <div id="myMeetingContainer" />
+      <div ref={elementRef} />
     </div>
   );
 };
