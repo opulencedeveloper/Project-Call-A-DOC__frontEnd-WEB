0561cca4e12998423ff0104d57addef3733d1766
opulencedeveloper <victorkudos@gmail.com>
2023-07-22 00:58:51 +0100

    header padding update

--- components/dashboard/dashboard-layout/DashBoardLayout.js
+++ components/dashboard/dashboard-layout/DashBoardLayout.js
@@ -3,7 +3,7 @@ import DashBoardNavigation from "./DashBoardNavigation";
 const DashBoardLayout = (props) => {
   const {type} = props;
   return (
-    <div className="flex pt-8">
+    <div className="flex pt-3 md:pt-8">
      <div> <DashBoardNavigation type={type}/> </div>
      <div className="flex flex-col w-full px-5 lg:flex-row 2xl:pl-14 2xl:pr-auto">
       {props.children}
